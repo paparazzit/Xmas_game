982d4b84ae7c145ccd5bf6db1335a336059ec55e
paparazzit <paparazzit@fgmail.com>
2022-09-07 14:02:10 +0200

    game structure

--- js/Game.js
+++ js/Game.js
@@ -1,60 +1,81 @@
 function Game(opt) {
+	this.hero = opt.hero;
 	this.inventory = opt.inventory;
-	this.inventoryCont = document.querySelector(".sidebar");
-
+	this.activeItem = null;
+	this.width = window.innerWidth;
+	this.height = window.innerHeight;
+	this.logMessages = {
+		msg0: `Hola, I'm Catalonian Christmas log...`,
+		msg1: "I'm part of some unique Christmas traditions",
+		msg2: "What you have there...",
+		msg3: "A hat, some candy, blanket...nice!",
+		stick: "Oh no... what you have there... a stick... put it back please",
+	};
+	this.gameStates = {
+		placeHat: false,
+		putBlanket: false,
+		feedCandy: false,
+		beatTheLog: false,
+	};
+	this.candyFed = 0;
+	this.beatingLog = 0;
+	// HTML ELEMENTS
+	this.sidebar = document.querySelector(".sidebar");
+	this.dropPos = document.querySelectorAll(".item-pos");
+	this.container = document.querySelector(".container");
 	this.cursorElement = document.querySelector(".cursor");
 	this.gamePanel = document.querySelector(".game-panel");
-	this.container = document.querySelector(".container");
-	this.dropPos = document.querySelectorAll(".item_pos");
+	this.mainImage = document.querySelector(".main-img");
+	this.logClicks = 1;
+	// DIMENSION RESET
 	this.leftReset = this.container.offsetLeft;
 	this.topReset = this.container.offsetTop;
-	this.width = window.innerWidth;
-	this.activeItem;
 
-	this.gameState = 0;
-	// KREIRAJ INVENTAR
 	this.createInventory = function () {
 		let text = "";
-		let i = 0;
-
-		for (prop in this.inventory) {
+		for (let prop in this.inventory) {
 			this[prop] = this.inventory[prop];
-			text += `<div class="thumb" data-index="${i}" data-item="${prop}">
-            <img src="${this.inventory[prop]}" alt=""/>
-            </div>`;
-			i++;
+			text += `<div class="thumb" data-destination="${this[prop].destination}" data-item="${prop}"><img src="${this[prop].img}" alt=""></div>`;
+			console.log(this[prop].img);
 		}
-		this.inventoryCont.innerHTML = text;
+		text += '<div class="thumb" data-item="drop" ><p>DROP</p></div>';
+		this.sidebar.innerHTML = text;
 		this.items = document.querySelectorAll(".thumb");
 	};
 
-	// DATEKTUJ KLIKTANJE NA INVETAR
 	this.itemsClicked = function (ev) {
-		let clicked = ev.target.getAttribute("data-item");
-		let index = ev.target.getAttribute("data-index");
-
-		this.items.forEach((item) => {
-			item.classList.remove("activated");
-		});
-		if (clicked === this.activeItem) {
+		console.log(ev.target);
+		if (ev.target.className !== "thumb") {
+			return;
+		}
+		if (ev.target.getAttribute("data-item") === "drop" && this.activeItem) {
 			this.dropItem();
 			return;
-		} else {
-			this.activeItem = clicked;
-			this.items[index].classList.add("activated");
-			this.cursorElement.innerHTML = `<img src ="${
-				this[this.activeItem]
-			}" alt=""/>`;
-			if (this.width > 521) {
-				this.cursorElement.classList.add("active");
-				this.cursorElement.style.left = ev.clientX - this.leftReset + "px";
-				this.cursorElement.style.top = ev.clientY - this.topReset + "px";
-				this.trackItem();
-			}
-			this.checkGameState();
+		}
+		if (ev.target.getAttribute("data-item") !== "drop") {
+			this.activeItem = {
+				name: ev.target.getAttribute("data-item"),
+				destination: ev.target.getAttribute("data-destination"),
+				img: ev.target.children[0].getAttribute("src"),
+			};
+
+			this.createCursorElement(ev);
+			return;
 		}
 	};
 
+	this.createCursorElement = function (ev) {
+		this.cursorElement.innerHTML = `<img src="${this.activeItem.img}">`;
+
+		this.cursorElement.classList.add("active");
+		// position element on click
+		if (this.width > 521) {
+			this.cursorElement.style.left = ev.clientX - this.leftReset + "px";
+			this.cursorElement.style.top = ev.clientY - this.topReset + "px";
+			this.trackItem();
+		}
+	};
+	// TRACK ITEM
 	this.trackItem = function () {
 		window.addEventListener("mousemove", (ev) => {
 			this.cursorElement.style.left = ev.clientX - this.leftReset + "px";
@@ -62,157 +83,160 @@ function Game(opt) {
 		});
 	};
 
-	// UPOTREBI ITEM
-	this.checkItem = function (posName) {
-		if (!this.activeItem) {
-			console.log("nemam nista");
-			return;
-		}
-		if (posName === this.activeItem) {
-			console.log(this.checkGameState());
-		} else if (
-			posName === "mouth" &&
-			(this.activeItem === "bonBon" ||
-				this.activeItem === "cane" ||
-				this.activeItem === "lolly")
-		) {
-			console.log(this.checkGameState());
-		} else {
-			console.log("wrong Item");
-		}
-	};
-	// IZBACUEM ITEM
+	// DROPING ITEM
 	this.dropItem = function () {
+		if (this.gameStates.putBlanket) {
+			console.log("BACI ITEM");
+			window.removeEventListener("dblclick", this.dropItem, true);
+		}
 		this.activeItem = null;
 		this.cursorElement.innerHTML = "";
-		this.cursorElement.classList.remove("active");
-		this.items.forEach((item) => {
-			item.classList.remove("activated");
-		});
 	};
-	this.fed = 0;
-	this.useItem = function (ev) {
-		// console.log(this);
+
+	// MAIN IMAGE CLICKS
+	this.mainImageClicks = function (ev) {
+		if (
+			!this.activeItem &&
+			this.logMessages[`msg${this.logClicks}`] &&
+			!this.gameStates.placeHat
+		) {
+			console.log(this.logMessages[`msg${this.logClicks}`]);
+			this.logClicks++;
+			return;
+		}
+		if (
+			this.activeItem &&
+			this.activeItem.name === "stick" &&
+			this.logMessages.stick &&
+			!this.gameStates.putBlanket
+		) {
+			console.log(this.logMessages.stick);
+			delete this.logMessages.stick;
+			return;
+		}
+		if (this.activeItem && ev.target.getAttribute("data-item")) {
+			this.checkItem(ev);
+			return;
+		}
 
 		if (
-			ev.target.getAttribute("data-item") === "mouth" &&
-			(this.activeItem === "lolly" ||
-				this.activeItem === "cane" ||
-				this.activeItem === "bonBon")
+			this.gameStates.putBlanket &&
+			this.activeItem &&
+			this.activeItem.name === "stick" &&
+			!this.gameStates.beatTheLog
 		) {
-			if (
-				this.fed <= 2 &&
-				this.dropPos[this.gameState].getAttribute("data-listen") === "true"
-			) {
-				console.log("game state", this.gameState);
-				this.dropItem();
-				this.fed++;
-				if (this.fed === 3) {
-					this.gameState++;
-					console.log("DOSTA", this.gameState);
-					this.checkGameState();
-					return;
-				}
+			this.beatingLog++;
+			if (this.beatingLog > 6) {
+				console.log("FINISH");
+				this.gameStates.beatTheLog = true;
+				return;
 			}
-		}
-		if (ev.target.getAttribute("data-item") === this.activeItem) {
-			console.log(this.dropPos[this.gameState]);
-
-			this.dropItem();
-			this.gameState++;
+			console.log("LUPAJ BRATE ", this.beatingLog);
 
 			return;
 		}
 	};
 
-	this.init = function () {
-		console.log(this.stick);
+	// CHEKING ITEMS
+	this.checkItem = function (ev) {
+		if (
+			!this.gameStates.placeHat &&
+			this.activeItem.name === "hat" &&
+			ev.target.getAttribute("data-item") === "hat"
+		) {
+			console.log("spustam kapicu");
+			this.gameStates.placeHat = true;
+			this.useItem(ev.target);
+			return;
+		}
+		if (
+			this.gameStates.placeHat &&
+			!this.gameStates.feedCandy &&
+			this.activeItem.destination === "mouth" &&
+			ev.target.getAttribute("data-item") === "mouth" &&
+			this.candyFed !== 3
+		) {
+			this.candyFed++;
+			console.log("DAJEM CANDY", this.activeItem, " fedd: ", this.candyFed);
+			this.checkCandy();
+			this.dropItem();
+			return;
+		}
+		if (
+			this.gameStates.feedCandy &&
+			this.activeItem.name === "blanket" &&
+			ev.target.getAttribute("data-item") === "blanket" &&
+			!this.gameStates.putBlanket
+		) {
+			console.log("OBUCI CEBE");
+			this.useItem(ev.target, "/assets/blanket2.png");
+			this.gameStates.putBlanket = true;
+			this.dropItem();
+			return;
+		}
 	};
-	this.checkGameState = function () {
-		console.log(this.gameState);
-		let posName;
-		switch (this.gameState) {
-			case 0:
-				console.log("stavljam kapu");
-				this.dropPos[this.gameState].classList.add("activated");
-				posName = this.dropPos[this.gameState].getAttribute("data-item");
-				this.dropPos[this.gameState].addEventListener(
-					"click",
-					this.useItem.bind(this)
-				);
-				break;
-			case 1:
-				console.log("stavljam krpu");
-
-				this.dropPos[this.gameState - 1].removeEventListener(
-					"click",
-					this.useItem.bind(this)
-				);
-				this.dropPos[this.gameState].classList.add("activated");
-				posName = this.dropPos[this.gameState].getAttribute("data-item");
-				this.dropPos[this.gameState].addEventListener(
-					"click",
-					this.useItem.bind(this)
-				);
-
-				break;
-			case 2:
-				console.log("stavljam hranim");
-				this.dropPos[this.gameState - 1].removeEventListener(
-					"click",
-					this.useItem.bind(this)
-				);
-				this.dropPos[this.gameState].classList.add("activated");
-				posName = this.dropPos[this.gameState].getAttribute("data-item");
-				this.dropPos[this.gameState].addEventListener(
-					"click",
-					this.useItem.bind(this)
-				);
-				this.dropPos[this.gameState].setAttribute("data-listen", true);
-				break;
-			case 3:
-				this.dropPos[this.gameState - 1].removeEventListener(
-					"click",
-					this.useItem.bind(this)
-				);
-				console.log("TUCI");
-				break;
-			default:
-			// code block
+	this.checkCandy = function () {
+		if (this.candyFed === 3) {
+			console.log("NAHRANIO SAM DRVO");
+			this.gameStates.feedCandy = true;
 		}
 	};
 
-	// EVENTS
-
-	this.events = function () {
-		this.items.forEach((item) => {
-			item.addEventListener("click", this.itemsClicked.bind(this));
-		});
-		window.addEventListener("resize", (e) => {
-			this.width = window.innerWidth;
-			this.leftReset = this.container.offsetLeft;
-			this.topReset = this.container.offsetTop;
-		});
-		// this.dropPos.forEach((pos) => {
-		// 	let posName = pos.getAttribute("data-item");
-		// 	pos.addEventListener("click", this.checkItem.bind(this, posName));
-		// });
+	this.useItem = function (target, img) {
+		console.log(target);
+		console.log(this.activeItem.img);
+		let imgSrc = img;
+		if (!img) {
+			imgSrc = this.activeItem.img;
+		}
+		target.innerHTML = `<img src="${imgSrc}" alt=""/>`;
+		this.dropItem();
 	};
+
+	// #REGION EVENT
+	this.events = function () {
+		this.sidebar.addEventListener("click", this.itemsClicked.bind(this));
+		window.addEventListener("resize", () => {
+			this.width = window.innerWidth;
+			this.height = window.innerHeight;
+		});
+
+		this.mainImage.addEventListener("click", this.mainImageClicks.bind(this));
+	};
+	// #END REGION
 }
 
-let options = {
+let opt = {
 	hero: "/assets/log.png",
 	inventory: {
-		hat: "/assets/hat.png",
-		blanket: "/assets/blanket_thumb.png",
-		stick: "/assets/stick.png",
-		lolly: "/assets/lolly.png",
-		bonBon: "/assets/bonbon.png",
-		cane: "/assets/cane.png",
+		hat: {
+			img: "/assets/hat.png",
+			destination: "hat",
+		},
+		stick: {
+			img: "/assets/stick.png",
+			destination: "log",
+		},
+		blanket: {
+			img: "/assets/blanket_thumb.png",
+			destination: "blanket",
+		},
+		lolly: {
+			img: "/assets/lolly.png",
+			destination: "mouth",
+		},
+
+		bonBon: {
+			img: "/assets/bonbon.png",
+			destination: "mouth",
+		},
+		cane: {
+			img: "/assets/cane.png",
+			destination: "mouth",
+		},
 	},
 };
 
-let game = new Game(options);
+let game = new Game(opt);
 game.createInventory();
-
 game.events();
